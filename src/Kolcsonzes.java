acc994c3dfa744cb85a300002a8422ec2f42d0f2
MrMancs <marcell.szatmari2007@gmail.com>
2025-09-19 08:05:17 +0200

    Solve task 3

--- src/Kolcsonzes.java
+++ src/Kolcsonzes.java
@@ -53,4 +53,21 @@ public class Kolcsonzes {
     public void setVisszaPerc(int visszaPerc) {
         this.visszaPerc = visszaPerc;
     }
+
+    public Kolcsonzes() {
+        // TODO - test field usage
+    }
+
+    public Kolcsonzes(String nev, char jarmu, int elvitelOra, int elvitelPerc, int visszaOra, int visszaPerc){
+        setNev(nev);
+        setJarmu(jarmu);
+        setElvitelOra(elvitelOra);
+        setElvitelPerc(elvitelPerc);
+        setVisszaOra(visszaOra);
+        setVisszaPerc(visszaPerc);
+    }
+
+    public Kolcsonzes(String fileLine) {
+        // TODO - parse read line
+    }
 }
